2d6b8f60d9445cf06b48f7381a5b070f730aded9
cotsao <codytsao@gmail.com>
2022-01-12 15:38:49 -0800

    gtag

--- plugins/gtag.js
+++ plugins/gtag.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueGtag from 'vue-gtag'
-Vue.use(VueGtag, {
-    config: { id: 'G-5S2HKBQK7X' },
-    appName: 'cotsao_portfolio',
-});
+
+export default ({ app }) => {
+    Vue.use(VueGtag, {
+      config: { id: 'G-5S2HKBQK7X' },
+      appName: 'cotsao_portfolio',
+    }, app.router);
+  }
