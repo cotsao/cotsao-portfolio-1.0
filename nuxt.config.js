45b8367835843e1d1ea215073f9d169fa50bdb07
cotsao <codytsao@gmail.com>
2022-01-12 14:58:59 -0800

    gtag added

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,12 @@ export default {
   css: ["~/assets/main.css"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [
+    {
+      src: './plugins/gtag.js',
+      mode: 'client'
+  }
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
---
+++ plugins/gtag.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import VueGtag from 'vue-gtag'
+Vue.use(VueGtag, {
+    config: { id: '3145387502' },
+    appName: 'cotsao_portfolio',
+});
