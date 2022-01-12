c58cc9ed9d8302bc29162fde9f9234f5092b03eb
cotsao <codytsao@gmail.com>
2022-01-12 15:12:57 -0800

    id change

--- plugins/gtag.js
+++ plugins/gtag.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueGtag from 'vue-gtag'
 Vue.use(VueGtag, {
-    config: { id: '3145387502' },
+    config: { id: 'G-5S2HKBQK7X' },
     appName: 'cotsao_portfolio',
 });
